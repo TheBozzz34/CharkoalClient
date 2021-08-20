99c3aab414b460c2ef84b8707d5501fa50915a68
Ethan <lskywalker416@gmail.com>
2021-08-19 20:39:04 -0600

    Add Client

---
+++ src/main/java/company/fourleafclover/TwitterClient/Main.java
@@ -0,0 +1,77 @@
+package company.fourleafclover.TwitterClient;
+
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Started!");
+
+
+        final JFrame frame = new JFrame("Twitter Client");
+
+        JLabel lblKey = new JLabel("API:");
+        JTextField tfKey = new JPasswordField(20);
+        lblKey.setLabelFor(tfKey);
+
+        JLabel lblSecret = new JLabel("API Secret:");
+        JTextField tfSecret = new JPasswordField(20);
+        lblSecret.setLabelFor(tfSecret);
+
+        JLabel lblToken = new JLabel("Access Token:");
+        final JPasswordField pfToken = new JPasswordField(20);
+        lblToken.setLabelFor(pfToken);
+
+        JButton btnGet = new JButton("Send tweet");
+        btnGet.addActionListener(
+                new ActionListener() {
+
+                    public void actionPerformed(ActionEvent e) {
+                        String password = new String(pfToken.getPassword());
+                        JOptionPane.showMessageDialog(frame,
+                                "Tweet sent!");
+                    }
+                });
+
+        JButton btnLogin = new JButton("Login");
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new SpringLayout());
+
+        panel.add(lblKey);//API Key
+        panel.add(tfKey);//Api Key
+        panel.add(lblToken);//API Token
+        panel.add(pfToken);//API Token
+        panel.add(lblSecret);//Access Secret
+        panel.add(tfSecret);//Access Secret
+        panel.add(btnLogin);
+        panel.add(btnGet);
+
+        jtextfielddemo.SpringUtilities.makeCompactGrid(panel,
+                4, 2, //rows, cols
+                6, 6, //initX, initY
+                6, 6); //xPad, yPad
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 200);
+        frame.getContentPane().add(panel);
+        frame.setVisible(true);
+
+
+
+
+
+
+
+
+
+    }
+
+}
+
+
+
